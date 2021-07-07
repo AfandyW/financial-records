2a6c0ba6712bd4eac3304c01405de9cb2ddf4c1f
AfandyW <afandywibowo2000@gmail.com>
2021-07-07 10:45:06 +0800

    Create new Add Transaction

--- main.go
+++ main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 )
 
 type Transaction struct {
-	Id              uint   `json:"id"`
-	Title           string `json:"title"`
-	Description     string `json:"description"`
-	TypeTransaction string `json:"type_transaction"`
-	Amount          int    `json:"amount"`
-	Currency        string `json:"currency"`
-	Category        string `json:"category"`
-	SubCategory     string `json:"sub_category"`
-	TransactionAt   string `json:"transaction_at"`
-	CreateAt        string `json:"create_at"`
-	UpdateAt        string `json:"update_at"`
+	Id              string    `json:"id"`
+	Title           string    `json:"title"`
+	Description     string    `json:"description"`
+	TypeTransaction string    `json:"type_transaction"`
+	Amount          int       `json:"amount"`
+	Currency        string    `json:"currency"`
+	Category        string    `json:"category"`
+	SubCategory     string    `json:"sub_category"`
+	TransactionAt   time.Time `json:"transaction_at"`
+	CreateAt        time.Time `json:"create_at"`
+	UpdateAt        time.Time `json:"update_at"`
 }
 
 var Transactions []Transaction
@@ -27,6 +30,7 @@ func main() {
 
 	route.Get("/", HomeController)
 	route.Get("/transactions", GetAllTransactionsController)
+	route.Post("/transactions", AddTransactionsController)
 
 	http.ListenAndServe(":3000", route)
 
@@ -37,5 +41,24 @@ func HomeController(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetAllTransactionsController(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(Transactions)
+}
 
+func AddTransactionsController(w http.ResponseWriter, r *http.Request) {
+	var newTransaction Transaction
+
+	err := json.NewDecoder(r.Body).Decode(&newTransaction)
+
+	if err != nil {
+		http.Error(w, "Error read body request", http.StatusInternalServerError)
+	}
+	timeNow := time.Now()
+	newTransaction.Id = uuid.New().String()
+	newTransaction.TransactionAt = timeNow.Local()
+	newTransaction.CreateAt = newTransaction.TransactionAt
+	newTransaction.UpdateAt = newTransaction.TransactionAt
+
+	Transactions = append(Transactions, newTransaction)
+
+	json.NewEncoder(w).Encode(Transactions)
 }
