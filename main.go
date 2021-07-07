8f92a1eb6aa3e758bd23fcc92723a18177c41c41
AfandyW <afandywibowo2000@gmail.com>
2021-07-07 10:06:53 +0800

    create transaction data

--- main.go
+++ main.go
@@ -6,11 +6,27 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+type Transaction struct {
+	Id              uint   `json:"id"`
+	Title           string `json:"title"`
+	Description     string `json:"description"`
+	TypeTransaction string `json:"type_transaction"`
+	Amount          int    `json:"amount"`
+	Currency        string `json:"currency"`
+	Category        string `json:"category"`
+	SubCategory     string `json:"sub_category"`
+	TransactionAt   string `json:"transaction_at"`
+	CreateAt        string `json:"create_at"`
+	UpdateAt        string `json:"update_at"`
+}
+
+var Transactions []Transaction
+
 func main() {
 	route := chi.NewRouter()
 
 	route.Get("/", HomeController)
-	route.Get("/records-transaction", RecordsController)
+	route.Get("/transactions", GetAllTransactionsController)
 
 	http.ListenAndServe(":3000", route)
 
@@ -20,6 +36,6 @@ func HomeController(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hay, Welcome"))
 }
 
-func RecordsController(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Get All recors"))
+func GetAllTransactionsController(w http.ResponseWriter, r *http.Request) {
+
 }
