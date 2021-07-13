d0abd024561fd450914c3e3fafbaf5363d2401cb
AfandyW <afandywibowo2000@gmail.com>
2021-07-13 15:50:18 +0800

    create new routes

---
+++ controllers/routes.go
@@ -0,0 +1,10 @@
+package controllers
+
+func (s *Server) initializeRoutes() {
+	s.Router.Get("/", s.HomeController)
+	s.Router.Get("/transactions", s.GetAllTransactionsController)
+	s.Router.Post("/transactions", s.AddTransactionController)
+	s.Router.Get("/transactions/{id}", s.GetTransactionController)
+	s.Router.Put("/transactions/{id}", s.EditTransactionController)
+	s.Router.Delete("/transactions/{id}", s.DeleteTransactionController)
+}
