feaaf4394e7a7c2c99f5e0d4bb73f9ea16ad7187
afandy2000 <afandywibowo2000@gmail.com>
2021-08-16 00:29:53 +0800

    Add new, sql migration

---
+++ migration/transaction.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE financial;
+CREATE TABLE transactions (
+    id VARCHAR(50) PRIMARY KEY,
+    title VARCHAR(100),
+    description VARCHAR(200),
+    type_transaction VARCHAR(10),
+    amount integer,
+    currency VARCHAR(50),
+    category VARCHAR(50),
+    sub_category VARCHAR(100),
+    transaction_at TIMESTAMP NOT NULL,
+    create_at TIMESTAMP NOT NULL,
+    update_at TIMESTAMP NOT NULL,
+);
