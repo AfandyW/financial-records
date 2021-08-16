ee7236c65e8b93837e7864ca15f4948be06b3070
afandy2000 <afandywibowo2000@gmail.com>
2021-08-16 11:06:04 +0800

    add .env setup

--- main.go
+++ main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"log"
+	"os"
+	"strconv"
+
 	"github.com/AfandyW/financial-records/controllers"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var server = controllers.Server{}
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "mapple"
-	password = "afandy"
-	dbname   = "financial"
-)
-
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	host := os.Getenv("HOST")
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	dbname := os.Getenv("DBNAME")
 	server.Initialize(port, user, password, host, dbname)
 	server.Run(":3000")
 }
