40adc1b4fb24687b6ca2dee69f581dd2a4e64170
afandy2000 <afandywibowo2000@gmail.com>
2021-08-17 11:03:52 +0800

    refactoring db instance

--- controllers/server.go
+++ controllers/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -12,22 +13,30 @@ import (
 type Server struct {
 	DB     *sql.DB
 	Router *chi.Mux
+	initOnce sync.Once
+}
+
+func (server *Server)newDB(psqlconn string,DBName string){
+	var err error
+
+	server.initOnce.Do(func() {
+		if server.DB == nil {
+			server.DB, err = sql.Open("postgres", psqlconn)
+
+			if err != nil {
+				fmt.Printf("Cannot connect to %s database", DBName)
+				log.Fatal(" Error : ", err)
+			} else {
+				fmt.Printf("Connected to %s database", DBName)
+			}
+		}
+	})
 }
 
 func (server *Server) Initialize(DBPort int, DBUser, DBPassword, DBHost, DBName string) {
-	var err error
-
-	// Open the connection
 	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", DBHost, DBPort, DBUser, DBPassword, DBName)
-
-	server.DB, err = sql.Open("postgres", psqlconn)
-
-	if err != nil {
-		fmt.Printf("Cannot connect to %s database", DBName)
-		log.Fatal(" Error : ", err)
-	} else {
-		fmt.Printf("Connected to %s database", DBName)
-	}
+	// Open the connection
+	server.newDB(psqlconn,DBName)
 
 	server.Router = chi.NewRouter()
 	server.initializeRoutes()
